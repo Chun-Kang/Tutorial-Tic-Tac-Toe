6136eb64489a825fe2b0964dba32ddee77c6ce8b
Chun Kang <chun@miniasp.com>
2023-09-28 16:12:37 +0800

    Completed game.

--- src/App.tsx
+++ src/App.tsx
@@ -9,41 +9,144 @@ import './style.css';
 //   )
 // }
 
-function Square() {
-  const  [value, setValue] = useState(null);
+function Square({ value, onSquareClick }) {
+  // const  [value, setValue] = useState(null);
 
-  function handleClick() {
-    console.log('click');
-  }
+  // function handleClick() {
+  //   setValue('X');
+  // }
 
   return (
-    <button className="square" onClick={handleClick}>
+    // <button className="square" onClick={handleClick}>
+    //   {value}
+    // </button>
+    <button className="square" onClick={onSquareClick}>
       {value}
     </button>
+
   );
 }
 
-export default function Board() {
+function Board({ xIsNext, squares, onplay }) {
+  const winner = calculateWinner(squares);
+  let status: string;
+
+  function handleClick(i: number) {
+    // 已經有值了或是已經勝利，就不要再設定了
+    if (squares[i] || winner) {
+      return;
+    }
+
+    // 重新取得一個新的陣列
+    const nextSquares = squares.slice();
+
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+
+    onplay(nextSquares);
+  }
+
+  if (winner) {
+    status = `Winner: ${winner}`;
+  } else {
+    status = `Next player: ${xIsNext ? 'X' : 'O'}`
+  }
+
   return (
     <>
+      <div className="status">{status}</div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
   );
 }
+
+export default function Game() {
+  // const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
+
+  function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+    // setXIsNext(!xIsNext);
+  }
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+    // setXIsNext(nextMove % 2 === 0);
+  }
+
+  const moves = history.map((_, move) => {
+    let description: string;
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  })
+
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onplay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
+    </div>)
+}
+
+function calculateWinner(squares: string[]) {
+  // 九宮格的所有可能連線
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+
+    const [a, b, c] = lines[i];
+
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
 
 // export const App: FC<{ name: string }> = ({ name }) => {
 //   const  [count, setCount] = useState(0);
